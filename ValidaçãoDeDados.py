d9174ed74d01524d55dbb5be18f7ba5ad4d094ef
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-11-20 17:41:53 -0300

    Update ValidaçãoDeDados.py

--- ValidaçãoDeDados.py
+++ ValidaçãoDeDados.py
@@ -3,5 +3,5 @@ print('='*12,' Validação de Dados ','='*12)
 sexo = str(input('Informe seu sexo [M/F]: ')).strip().upper()[0]
 while sexo not in 'MmFf':
     sexo = str(input('Dados Inválidos. Por favor, Informe seu sexo: ')).strip().upper()[0]
-print('\nSexo {} registrado com sucesso!'.format(sexo))
+print('='*46,'\nSexo {} registrado com sucesso!'.format(sexo))
 print('='*46)

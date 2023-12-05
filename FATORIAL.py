26cd1abfc9f88d9af2bed41383c444d62a8239c7
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-12-05 16:12:39 -0300

    Create FATORIAL.py
    Fatorial Ultilizando Módulo

---
+++ FATORIAL.py
@@ -0,0 +1,6 @@
+print('=-='* 4,' Calculo do Fatorial ','=-='*4)
+from math import factorial
+n = int(input('Digite um número para calcular o Fatorial: '))
+f = factorial(n)
+print('O FATORIAL de {} é {}'.format(n, f))
+print('=-='* 16)
